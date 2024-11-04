97d95fa17e5401e1a5fc764cadfb2a50647a3096
Muhib Dulquer <m.dulquer.sumon@gmail.com>
2024-11-05 01:06:43 +0600

    developing the crud logic

--- programs/crud/src/lib.rs
+++ programs/crud/src/lib.rs
@@ -6,11 +6,35 @@ declare_id!("Mx2KcqEtge1DbkVTNC3oyM1Bmbvd8eXfADUWxegdvGy");
 pub mod crud {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_journal_state(ctx: Context<CreateEntry>, title:String) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key.as_ref()],
+        bump,
+         space = 8 + JournalEntryState::INIT_SPACE,
+         payer = owner,
+    )]
+    pub journal_entry : Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner:Signer<'info>,
+    pub system_program:Program<'info,System>
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState{
+    pub owner: PubKey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String,
+
+}
