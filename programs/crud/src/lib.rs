7e70a844837b41d8837f49e6ca55a99a74ce6a1b
Muhib Dulquer <m.dulquer.sumon@gmail.com>
2024-11-05 13:04:53 +0600

    completed CRUD program

--- programs/crud/src/lib.rs
+++ programs/crud/src/lib.rs
@@ -6,8 +6,21 @@ declare_id!("Mx2KcqEtge1DbkVTNC3oyM1Bmbvd8eXfADUWxegdvGy");
 pub mod crud {
     use super::*;
 
-    pub fn create_journal_state(ctx: Context<CreateEntry>, title:String) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_journal_state(ctx: Context<CreateEntry>, title:String, message:String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.message = message;
+        Ok(())
+    }
+
+    pub fn update_journal_entry(ctx:Context<UpdateEntry>, _title:String, message:String)-> Result<()>{
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
+        Ok(())
+    }
+    pub fn delete_journal_entry(_ctx:Context<DeleteEntry>, _title:String)-> Result<()>{
+        
         Ok(())
     }
 }
@@ -27,11 +40,42 @@ pub struct CreateEntry<'info> {
     pub owner:Signer<'info>,
     pub system_program:Program<'info,System>
 }
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key.as_ref()],
+        bump,
+         realloc = 8 + JournalEntryState::INIT_SPACE,
+         realloc::payer = owner,
+         realloc::zero = true,
+    )]
+    pub journal_entry : Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner:Signer<'info>,
+    pub system_program:Program<'info,System>
+}
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key.as_ref()],
+        bump,
+        close = owner,
+
+    )]
+    pub journal_entry : Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner:Signer<'info>,
+    pub system_program:Program<'info,System>
+}
 
 #[account]
 #[derive(InitSpace)]
 pub struct JournalEntryState{
-    pub owner: PubKey,
+    pub owner: Pubkey,
     #[max_len(50)]
     pub title: String,
     #[max_len(1000)]
